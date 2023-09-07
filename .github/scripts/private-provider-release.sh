aee0e88cd7217cf3e2b02f0d3358e7a28d4eab34
Lion Chen <lion.chen@hashicorp.com>
2023-09-07 14:28:38 -0600

    keep dist

--- .github/scripts/private-provider-release.sh
+++ .github/scripts/private-provider-release.sh
@@ -106,6 +106,8 @@ providerVersion=$(curl -s \
 
 platformsJson=$(cat dist/artifacts.json)
 
+echo "platformsJson:\n$platformsJson"
+
 shasumsUploadUrl=$(echo $providerVersion | jq -r '.data.links."shasums-upload"')
 shasumsSigUploadUrl=$(echo $providerVersion | jq -r '.data.links."shasums-sig-upload"')
 
